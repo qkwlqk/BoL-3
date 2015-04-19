8f77e579e918b59dded1ef0bf92168467f95169f
BolHTTF <bolhttf@naver.com>
2015-04-19 14:07:39 +0900

    fix Smite

--- HTTF/HTTF Corki.lua
+++ HTTF/HTTF Corki.lua
@@ -1,4 +1,4 @@
-local Version = "1.111"
+local Version = "1.112"
 local AutoUpdate = true
 
 if myHero.charName ~= "Corki" then
@@ -276,6 +276,7 @@ function CorkiMenu()
     
   Menu:addSubMenu("Jungle Steal Settings", "JSteal")
     Menu.JSteal:addParam("On", "Jungle Steal", SCRIPT_PARAM_ONKEYDOWN, false, GetKey('X'))
+    Menu.JSteal:addParam("On2", "Jungle Steal Toggle", SCRIPT_PARAM_ONKEYTOGGLE, true, GetKey('N'))
       Menu.JSteal:addParam("Blank", "", SCRIPT_PARAM_INFO, "")
     Menu.JSteal:addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
     Menu.JSteal:addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
@@ -301,13 +302,6 @@ function CorkiMenu()
     Menu.KillSteal:addParam("S", "Use Stalker's Blade", SCRIPT_PARAM_ONOFF, true)
     end
     
-  if Smite ~= nil then
-  Menu:addSubMenu("AutoCast Settings", "Auto")
-    Menu.Auto:addParam("On", "AutoCast", SCRIPT_PARAM_ONOFF, true)
-      Menu.Auto:addParam("Blank", "", SCRIPT_PARAM_INFO, "")
-    Menu.Auto:addParam("AutoS", "Use Smite", SCRIPT_PARAM_ONKEYTOGGLE, true, GetKey('N'))
-  end
-  
   Menu:addSubMenu("Flee Settings", "Flee")
     Menu.Flee:addParam("On", "Flee (Only Use KillSteal)", SCRIPT_PARAM_ONKEYDOWN, false, GetKey('G'))
     
@@ -439,7 +433,7 @@ function OnTick()
     LastHit()
   end
   
-  if Menu.JSteal.On then
+  if Menu.JSteal.On or Menu.JSteal.On2 then
     JSteal()
   end
   
@@ -1289,7 +1283,7 @@ function OnDraw()
     DrawCircle(myHero.x, myHero.y, myHero.z, I.range, ARGB(0xFF, 0xFF, 0x24, 0x24))
   end
   
-  if Menu.Draw.S and S.ready and (Menu.Auto.On and Menu.Auto.AutoS or Menu.JSteal.On and Menu.JSteal.S) then
+  if Menu.Draw.S and S.ready and (Menu.JSteal.On or Menu.JSteal.On2) and Menu.JSteal.S then
     DrawCircle(myHero.x, myHero.y, myHero.z, S.range, ARGB(0xFF, 0xFF, 0x14, 0x93))
   end
   
